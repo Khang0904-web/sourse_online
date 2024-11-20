02d20b0f5ba8fc599b09b8c8699c753e07f8dc7a
Khang0904-web <nguyenhoangkhang123ks@gmail.com>
2024-11-21 01:42:14 +0700

    model product

---
+++ App/Models/Product.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Models;
+
+class Product extends BaseModel
+{
+    protected $table = 'sourse';
+    protected $id = 'id';
+
+    public function getAllProduct()
+    {
+        return $this->getAll();
+    }
+    public function getOneProduct($id)
+    {
+        return $this->getOne($id);
+    }
+
+    public function createProduct($data)
+    {
+        return $this->create($data);
+    }
+    public function updateProduct($id, $data)
+    {
+        return $this->update($id, $data);
+    }
+
+    public function deleteProduct($id)
+    {
+        return $this->delete($id);
+    }
+    public function getAllProductByStatus()
+    {
+        $result = [];
+        try {
+            $sql = "SELECT sourse.* FROM sourse INNER JOIN categories ON sourse.category_id=categories.id WHERE sourse.status=" . self::STATUS_ENABLE . " AND categories.status=" . self::STATUS_ENABLE;
+            $result = $this->_conn->MySQLi()->query($sql);
+            return $result->fetch_all(MYSQLI_ASSOC);
+        } catch (\Throwable $th) {
+            error_log('Lỗi khi hiển thị tất cả dữ liệu: ' . $th->getMessage());
+            return $result;
+        }
+    }
+
+
+    public function getOneProductByName($name)
+    {
+        return $this->getOneByName($name);
+    }
+
+
+    public function getAllProductJoinCategory()
+    {
+        $result = [];
+        try {
+            $sql = "SELECT sourse.*, categories.name AS category_name 
+        FROM sourse 
+        INNER JOIN categories 
+        ON sourse.category_id = categories.id;";
+            $result = $this->_conn->MySQLi()->query($sql);
+            return $result->fetch_all(MYSQLI_ASSOC);
+        } catch (\Throwable $th) {
+            error_log('Lỗi khi hiển thị tất cả dữ liệu: ' . $th->getMessage());
+            return $result;
+        }
+    }
+
+    public function getAllProductByCategoryAndStatus(int $id)
+    {
+        $result = [];
+        try {
+            $sql = "SELECT sourse.*, categories.name AS category_name FROM sourse INNER JOIN categories ON sourse.category_id=categories.id WHERE sourse.status=" . self::STATUS_ENABLE . " AND categories.status=" . self::STATUS_ENABLE . " AND sourse.category_id=?";
+            $conn = $this->_conn->MySQLi();
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('i', $id);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        } catch (\Throwable $th) {
+            error_log('Lỗi khi hiển thị chi tiết dữ liệu: ' . $th->getMessage());
+            return $result;
+        }
+    }
+
+    public function getOneProductByStatus(int $id)
+    {
+        $result = [];
+        try {
+            $sql = "SELECT sourse.*, categories.name AS category_name FROM sourse INNER JOIN categories ON sourse.category_id=categories.id WHERE sourse.status=" . self::STATUS_ENABLE . " AND categories.status=" . self::STATUS_ENABLE . " AND sourse.id=?";
+            $conn = $this->_conn->MySQLi();
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('i', $id);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_assoc();
+        } catch (\Throwable $th) {
+            error_log('Lỗi khi hiển thị chi tiết dữ liệu: ' . $th->getMessage());
+            return $result;
+        }
+    }
+
+}
