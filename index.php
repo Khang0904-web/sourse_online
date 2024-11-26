bb3bf0ce860d27b391cb0d7fbeedac6053eb3a7c
Khang0904-web <nguyenhoangkhang123ks@gmail.com>
2024-11-26 23:30:10 +0700

    update index add lesson route

---
+++ index.php
@@ -0,0 +1,144 @@
+<?php
+use App\Helpers\AuthHelper;
+session_start();
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+ini_set('log_errors', TRUE); 
+
+//kiểm tra xem có file log chưa ? chưa: tạo file và lưu vào/ có: lưu vào luôn
+ini_set('error_log', './logs/php/php-errors.log');
+
+use App\Route;
+
+require_once 'vendor/autoload.php';
+
+require_once 'config.php';
+
+AuthHelper::middleware();
+
+// *** Client
+Route::get('/', 'App\Controllers\Client\HomeController@index');
+Route::get('/products', 'App\Controllers\Client\ProductController@index');
+Route::get('/products/{id}', 'App\Controllers\Client\ProductController@detail');
+Route::get('/products/categories/{id}', 'App\Controllers\Client\ProductController@getProductByCategory');
+Route::get('/register', 'App\Controllers\Client\AuthController@register');
+Route::post('/register', 'App\Controllers\Client\AuthController@registerAction');
+Route::get('/login', 'App\Controllers\Client\AuthController@login');
+Route::post('/login', 'App\Controllers\Client\AuthController@loginAction');
+Route::get('/logout', 'App\Controllers\Client\AuthController@logout');
+
+Route::get('/study', 'App\Controllers\Client\StudyController@study');
+Route::get('/Post', 'App\Controllers\Client\PostController@index');
+Route::get('/contact', 'App\Controllers\Client\ContactController@index');
+
+
+
+
+//sửa thông tin và xem người dùng
+Route::get('/users/{id}', 'App\Controllers\Client\AuthController@edit');
+Route::put('/users/{id}', 'App\Controllers\Client\AuthController@update');
+
+//thay đổi mật khẩu
+Route::get('/forgot-password', 'App\Controllers\Client\AuthController@forgotPassword');
+Route::post('/forgot-password', 'App\Controllers\Client\AuthController@forgotPasswordAction');
+
+//reset
+Route::get('/reset-password', 'App\Controllers\Client\AuthController@resetPassword');
+Route::put('/reset-password', 'App\Controllers\Client\AuthController@resetPasswordAction');
+Route::get('/change-password', 'App\Controllers\Client\AuthController@changePassword');
+
+
+
+// *** Admin
+
+Route::get('/admin', 'App\Controllers\Admin\HomeController@index');
+
+//  *** Category
+// GET /categories (lấy danh sách loại sản phẩm)
+Route::get('/admin/categories', 'App\Controllers\Admin\CategoryController@index');
+
+// GET /categories/create (hiển thị form thêm loại sản phẩm)
+Route::get('/admin/categories/create', 'App\Controllers\Admin\CategoryController@create');
+
+// POST /categories (tạo mới một loại sản phẩm)
+Route::post('/admin/categories', 'App\Controllers\Admin\CategoryController@store');
+
+// GET /categories/{id} (lấy chi tiết loại sản phẩm với id cụ thể)
+Route::get('/admin/categories/{id}', 'App\Controllers\Admin\CategoryController@edit');
+
+// PUT /categories/{id} (update loại sản phẩm với id cụ thể)
+Route::put('/admin/categories/{id}', 'App\Controllers\Admin\CategoryController@update');
+
+// DELETE /categories/{id} (delete loại sản phẩm với id cụ thể)
+Route::delete('/admin/categories/{id}', 'App\Controllers\Admin\CategoryController@delete');
+
+
+
+//  *** Product
+// GET /products (lấy danh sách sản phẩm)
+Route::get('/admin/products', 'App\Controllers\Admin\ProductController@index');
+
+// GET /products/create (hiển thị form thêm sản phẩm)
+Route::get('/admin/products/create', 'App\Controllers\Admin\ProductController@create');
+
+// POST /products (tạo mới một sản phẩm)
+Route::post('/admin/products', 'App\Controllers\Admin\ProductController@store');
+
+// GET /products/{id} (lấy chi tiết sản phẩm với id cụ thể)
+Route::get('/admin/products/{id}', 'App\Controllers\Admin\ProductController@edit');
+
+// PUT /products/{id} (update sản phẩm với id cụ thể)
+Route::put('/admin/products/{id}', 'App\Controllers\Admin\ProductController@update');
+
+// DELETE /products/{id} (delete sản phẩm với id cụ thể)
+Route::delete('/admin/products/{id}', 'App\Controllers\Admin\ProductController@delete');
+
+
+
+//  *** Lesson
+// GET /lesson (lấy danh sách sản phẩm)
+Route::get('/admin/lessons', 'App\Controllers\Admin\LessonController@index');
+
+// GET /lessons/create (hiển thị form thêm sản phẩm)
+Route::get('/admin/lessons/create', 'App\Controllers\Admin\LessonController@create');
+
+// POST /lessons (tạo mới một sản phẩm)
+Route::post('/admin/lessons', 'App\Controllers\Admin\LessonController@store');
+
+// GET /lessons/{id} (lấy chi tiết sản phẩm với id cụ thể)
+Route::get('/admin/lessons/{id}', 'App\Controllers\Admin\LessonController@edit');
+
+// PUT /lessons/{id} (update sản phẩm với id cụ thể)
+Route::put('/admin/lessons/{id}', 'App\Controllers\Admin\LessonController@update');
+
+// DELETE /lessons/{id} (delete sản phẩm với id cụ thể)
+Route::delete('/admin/lessons/{id}', 'App\Controllers\Admin\LessonController@delete');
+
+
+
+
+
+//  *** User
+// GET /users (lấy danh sách người dùng)
+Route::get('/admin/users', 'App\Controllers\Admin\UserController@index');
+
+// GET /users/create (hiển thị form thêm người dùng)
+Route::get('/admin/users/create', 'App\Controllers\Admin\UserController@create');
+
+// POST /users (tạo mới một người dùng)
+Route::post('/admin/users', 'App\Controllers\Admin\UserController@store');
+
+// GET /users/{id} (lấy chi tiết người dùng với id cụ thể)
+Route::get('/admin/users/{id}', 'App\Controllers\Admin\UserController@edit');
+
+// PUT /users/{id} (update người dùng với id cụ thể)
+Route::put('/admin/users/{id}', 'App\Controllers\Admin\UserController@update');
+
+// DELETE /users/{id} (delete người dùng với id cụ thể)
+Route::delete('/admin/users/{id}', 'App\Controllers\Admin\UserController@delete');
+
+
+
+
+Route::dispatch($_SERVER['REQUEST_URI']);
