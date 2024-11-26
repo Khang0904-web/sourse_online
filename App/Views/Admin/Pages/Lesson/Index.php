badfe92c237a3d0a1cc8ba361628d63e746fcac7
Khang0904-web <nguyenhoangkhang123ks@gmail.com>
2024-11-26 23:29:19 +0700

    index lesson

---
+++ App/Views/Admin/Pages/Lesson/Index.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Views\Admin\Pages\Lesson;
+use App\Views\BaseView;
+
+class Index extends BaseView
+{
+    public static function render($data = null)
+    {
+?>
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-12 d-flex no-block align-items-center">
+                        <h4 class="page-title">QUẢN LÝ BÀI HỌC</h4>
+                        <div class="ms-auto text-end">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="/admin">Trang chủ</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Danh sách loại bài học</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Danh sách bài học</h5>
+                                <?php
+                                if (count($data)) :
+                                ?>
+                                    <div class="table-responsive">
+                                        <table id="" class="table table-striped ">
+                                            <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                   
+                                                    <th>Tên</th>
+                                                   
+                                                    <th>Thuộc khóa</th>
+                                                    
+                                                    <th></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                foreach ($data as $item) :
+                                                ?>
+                                                    <tr>
+                                                        <td><?= $item['id'] ?></td>
+                                                        
+                                                        <td><?= $item['title_lesson'] ?></td>
+                                                       
+                                                        <td><?= $item['course_id'] ?></td>
+                                                        
+                                                        <td>
+                                                            <a href="/admin/lessons/<?= $item['id'] ?>" class="btn btn-primary ">Sửa</a>
+                                                            <form action="/admin/lessons/<?= $item['id'] ?>" method="post" style="display: inline-block;" onsubmit="return confirm('Chắc chưa?')">
+                                                                <input type="hidden" name="method" value="DELETE" id="">
+                                                                <button type="submit" class="btn btn-danger text-white">Xoá</button>
+                                                            </form>
+                                                        </td>
+                                                    </tr>
+                                                <?php
+                                                endforeach;
+
+
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                <?php
+                                else :
+
+                                ?>
+                                    <h4 class="text-center text-danger">Không có dữ liệu</h4>
+                                <?php
+                                endif;
+
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+
+
+    <?php
+    }
+}
